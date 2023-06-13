7f8230a0d4fb01c14edc621ff6cb2328ee674403
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-06-13 19:40:23 -0300

    create books class

---
+++ src/main/java/com/mrodrigochaves/bookshelf/model/Books.java
@@ -0,0 +1,12 @@
+package com.mrodrigochaves.bookshelf.model;
+
+public class Books {
+    
+    private Long id;
+    private String title;
+    private String author;
+    private String published;
+    private String description;
+    
+
+}
