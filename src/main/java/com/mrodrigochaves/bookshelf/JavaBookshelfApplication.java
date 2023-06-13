b59ef6a7b5db2c1e91e4f047c843aec45dd80695
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-06-12 23:38:07 -0300

    add src files

---
+++ src/main/java/com/mrodrigochaves/bookshelf/JavaBookshelfApplication.java
@@ -0,0 +1,13 @@
+package com.mrodrigochaves.bookshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBookshelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBookshelfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mrodrigochaves/bookshelf/JavaBookshelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrodrigochaves.bookshelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBookshelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
