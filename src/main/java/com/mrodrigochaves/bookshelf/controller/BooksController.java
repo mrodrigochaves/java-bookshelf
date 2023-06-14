9df001c708c785bfad8a301f3657a22d8fc530d0
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-06-14 20:48:41 -0300

    add getAll and getByName

--- src/main/java/com/mrodrigochaves/bookshelf/controller/BooksController.java
+++ src/main/java/com/mrodrigochaves/bookshelf/controller/BooksController.java
@@ -1,5 +1,38 @@
 package com.mrodrigochaves.bookshelf.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mrodrigochaves.bookshelf.dto.BooksDTO;
+import com.mrodrigochaves.bookshelf.service.BooksService;
+
+@RestController
+@RequestMapping("/books")
 public class BooksController {
+
+    @Autowired
+    private final BooksService service;
+
+    public BooksController(BooksService service){
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<BooksDTO>> getAll(){
+        List<BooksDTO> books = service.getAll();
+        return ResponseEntity.ok(books);
+    }
     
+    @GetMapping
+    public ResponseEntity<List<BooksDTO>> getByName(@RequestParam("name") String name) {
+    List<BooksDTO> books = service.getByName(name);
+    return ResponseEntity.ok(books);
+}
+
 }
