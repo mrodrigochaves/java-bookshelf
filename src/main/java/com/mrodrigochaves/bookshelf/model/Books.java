63c2799e49e3ad5383ef5fe3642df89bc46714bc
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-06-14 20:47:51 -0300

    update books params

--- src/main/java/com/mrodrigochaves/bookshelf/model/Books.java
+++ src/main/java/com/mrodrigochaves/bookshelf/model/Books.java
@@ -1,11 +1,31 @@
 package com.mrodrigochaves.bookshelf.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class Books {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
     private Long id;
+
+    @Column(name = "name", nullable = false)
     private String title;
+
+    @Column(name = "author", nullable = false)
     private String author;
+
+    @Column(name = "published", nullable = false)
     private String published;
+
+    @Column(name = "description", nullable = false)
     private String description;
     
 
