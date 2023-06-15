db2c2dbcf208810b94507d7664a145465a5e07e9
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-06-14 21:20:27 -0300

    update repository params

--- src/main/java/com/mrodrigochaves/bookshelf/repository/BooksRepository.java
+++ src/main/java/com/mrodrigochaves/bookshelf/repository/BooksRepository.java
@@ -1,5 +1,7 @@
 package com.mrodrigochaves.bookshelf.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,15 @@ import com.mrodrigochaves.bookshelf.model.Books;
 
 @Repository
 public interface BooksRepository extends JpaRepository<Books, Long>{
+
+    List<Books> findByTitle(List<Books> books);
+
+    List<Books> findByTitle(String title);
+
+    List<Books> findByAuthor(String author);
+
+    List<Books> findByPublished(String published);
+
+    List<Books> findByDescription(String description);
     
 }
