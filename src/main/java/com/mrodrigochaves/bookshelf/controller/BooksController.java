5142b48af70e71404dec2c84013df8c5a3a2a8ef
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-06-14 21:15:19 -0300

    update conrtroller params

--- src/main/java/com/mrodrigochaves/bookshelf/controller/BooksController.java
+++ src/main/java/com/mrodrigochaves/bookshelf/controller/BooksController.java
@@ -5,8 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mrodrigochaves.bookshelf.dto.BooksDTO;
@@ -16,23 +16,41 @@ import com.mrodrigochaves.bookshelf.service.BooksService;
 @RequestMapping("/books")
 public class BooksController {
 
-    @Autowired
     private final BooksService service;
 
-    public BooksController(BooksService service){
+    @Autowired
+    public BooksController(BooksService service) {
         this.service = service;
     }
 
     @GetMapping
-    public ResponseEntity<List<BooksDTO>> getAll(){
+    public ResponseEntity<List<BooksDTO>> getAll() {
         List<BooksDTO> books = service.getAll();
         return ResponseEntity.ok(books);
     }
-    
-    @GetMapping
-    public ResponseEntity<List<BooksDTO>> getByName(@RequestParam("name") String name) {
-    List<BooksDTO> books = service.getByName(name);
-    return ResponseEntity.ok(books);
-}
+
+    @GetMapping("/title/{title}")
+    public ResponseEntity<List<BooksDTO>> getByTitle(@PathVariable("title") String title){
+        List<BooksDTO> books = service.getByTitle(title);
+        return ResponseEntity.ok(books);
+    }
+
+    @GetMapping("/author/{author}")
+    public ResponseEntity<List<BooksDTO>> getByAuthor(@PathVariable("author") String author){
+        List<BooksDTO> books = service.getByAuthor(author);
+        return ResponseEntity.ok(books);
+    }
+
+    @GetMapping("/published/{published}")
+    public ResponseEntity<List<BooksDTO>> getByPublished(@PathVariable("published") String published){
+        List<BooksDTO> books = service.getByPublished(published);
+        return ResponseEntity.ok(books);
+    }
+
+    @GetMapping("/description/{description}")
+    public ResponseEntity<List<BooksDTO>> getByDescription(@PathVariable("description") String description){
+        List<BooksDTO> books = service.getByDescription(description);
+        return ResponseEntity.ok(books);
+    }
 
 }
