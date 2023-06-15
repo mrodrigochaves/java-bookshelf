0b32e25e2a22d4828ffdbe32a2da6e8b8d4dc46d
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-06-14 21:21:06 -0300

    update book service

--- src/main/java/com/mrodrigochaves/bookshelf/service/BooksService.java
+++ src/main/java/com/mrodrigochaves/bookshelf/service/BooksService.java
@@ -7,5 +7,13 @@ import com.mrodrigochaves.bookshelf.dto.BooksDTO;
 public interface BooksService {
 
     List<BooksDTO> getAll();
-    
+
+    List<BooksDTO> getByTitle(String title);
+
+    List<BooksDTO> getByAuthor(String author);
+
+    List<BooksDTO> getByPublished(String published);
+
+	List<BooksDTO> getByDescription(String description);
+
 }
