f6fcacee56f153f749755a18a0649c48cb900b78
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-06-16 21:00:18 -0300

    update config

--- src/main/java/com/mrodrigochaves/bookshelf/config/MapperConfig.java
+++ src/main/java/com/mrodrigochaves/bookshelf/config/MapperConfig.java
@@ -1,10 +1,15 @@
 package com.mrodrigochaves.bookshelf.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MapperConfig {
 
-
+@Bean
+public ModelMapper mapper(){
+    return new ModelMapper();
+}
     
 }
