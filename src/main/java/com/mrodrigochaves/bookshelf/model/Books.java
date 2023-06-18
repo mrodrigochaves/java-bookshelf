bb5b63c698137554400e77b0a16201acffd6c696
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-06-17 23:44:03 -0300

    update model

--- src/main/java/com/mrodrigochaves/bookshelf/model/Books.java
+++ src/main/java/com/mrodrigochaves/bookshelf/model/Books.java
@@ -16,17 +16,17 @@ public class Books {
     @Column(name = "id", nullable = false)
     private Long id;
 
-    @Column(name = "title", nullable = false)
-    private String title;
-
     @Column(name = "author", nullable = false)
     private String author;
+
+    @Column(name = "description", nullable = false)
+    private String description;
 
     @Column(name = "published", nullable = false)
     private String published;
 
-    @Column(name = "description", nullable = false)
-    private String description;
+    @Column(name = "title", nullable = false)
+    private String title;
     
 
 }
