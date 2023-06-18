0b9aca5aa7b493cfdb0712ee2cec325ac77c0829
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-06-17 23:46:44 -0300

    update repository

--- src/main/java/com/mrodrigochaves/bookshelf/repository/BooksRepository.java
+++ src/main/java/com/mrodrigochaves/bookshelf/repository/BooksRepository.java
@@ -1,25 +1,22 @@
 package com.mrodrigochaves.bookshelf.repository;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.mrodrigochaves.bookshelf.dto.BooksDTO;
 import com.mrodrigochaves.bookshelf.model.Books;
 
 @Repository
 public interface BooksRepository extends JpaRepository<Books, Long> {
 
-    List<Books> findByTitle(String title);
 
     List<Books> findByAuthor(String author);
 
-    List<Books> findByPublished(String published);
-
     List<Books> findByDescription(String description);
 
-    Optional<BooksDTO> delete(Long id);
+    List<Books> findByPublished(String published);
+
+    List<Books> findByTitle(String title);
 
 }
