6cb6d7bf617bfb053bba565e6f771bbcf81419cc
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-06-19 18:44:48 -0300

    resolving bugs

--- src/main/java/com/mrodrigochaves/bookshelf/dto/BooksDTO.java
+++ src/main/java/com/mrodrigochaves/bookshelf/dto/BooksDTO.java
@@ -2,29 +2,20 @@ package com.mrodrigochaves.bookshelf.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
-import lombok.NonNull;
 
 @Data
 public class BooksDTO {
 
-     public BooksDTO() {
-    
-    }
-
     @NotBlank
-    @NonNull
     private String title;   
 
     @NotBlank
-    @NonNull
     private String author;
 
     @NotBlank
-    @NonNull
     private String description;
 
      @NotBlank
-     @NonNull
     private String published;
 
      
