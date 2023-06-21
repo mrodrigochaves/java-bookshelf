50c9a79733368fe4c38219e049717d43b54b82f5
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-06-20 21:20:37 -0300

    change params dto

--- src/main/java/com/mrodrigochaves/bookshelf/dto/BooksDTO.java
+++ src/main/java/com/mrodrigochaves/bookshelf/dto/BooksDTO.java
@@ -1,8 +1,5 @@
 package com.mrodrigochaves.bookshelf.dto;
 
-import java.text.SimpleDateFormat;
-import java.text.ParseException;
-import java.util.Date;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -12,7 +9,7 @@ import lombok.Data;
 public class BooksDTO {
 
     @NotBlank
-    private String title;   
+    private String title;
 
     @NotBlank
     private String author;
@@ -21,15 +18,5 @@ public class BooksDTO {
     private String description;
 
     @NotNull
-    private Date published;
-
-    public void setPublished(String publishedDate) {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-        try {
-            this.published = dateFormat.parse(publishedDate);
-        } catch (ParseException e) {
-            // Lidar com a exceção caso a data não possa ser formatada corretamente
-            e.printStackTrace();
-        }
-    }
+    private String published;
 }
