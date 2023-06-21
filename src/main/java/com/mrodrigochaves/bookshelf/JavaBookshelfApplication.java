6ee7ab072c70801633fe371f7262af20b84805a4
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-06-21 19:02:42 -0300

    remove initDataBase

--- src/main/java/com/mrodrigochaves/bookshelf/JavaBookshelfApplication.java
+++ src/main/java/com/mrodrigochaves/bookshelf/JavaBookshelfApplication.java
@@ -1,15 +1,9 @@
 package com.mrodrigochaves.bookshelf;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
 
-import com.mrodrigochaves.bookshelf.model.Books;
-import com.mrodrigochaves.bookshelf.repository.BooksRepository;
 
-@Component
 @SpringBootApplication
 public class JavaBookshelfApplication {
 
@@ -17,22 +11,4 @@ public class JavaBookshelfApplication {
 		SpringApplication.run(JavaBookshelfApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner initDataBase(BooksRepository booksRepository) {
-		return args -> {
-
-			booksRepository.deleteAll();
-
-			Books bk = new Books();
-            bk.setTitle("Livro");
-            bk.setAuthor("Marcio");
-            bk.setDescription("Livro teste");
-            bk.setPublished("20/01/2002");
-
-            booksRepository.save(bk);
-
-		
-
-		};
-	}
 }
