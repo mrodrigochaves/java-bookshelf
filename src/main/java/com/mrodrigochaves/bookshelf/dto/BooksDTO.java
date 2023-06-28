53591b98661be5ec8c801e8828127d50a39cd8a3
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-06-28 15:25:56 -0300

    update DTO

--- src/main/java/com/mrodrigochaves/bookshelf/dto/BooksDTO.java
+++ src/main/java/com/mrodrigochaves/bookshelf/dto/BooksDTO.java
@@ -1,6 +1,5 @@
 package com.mrodrigochaves.bookshelf.dto;
 
-
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -12,6 +11,12 @@ public class BooksDTO {
     private String title;
 
     @NotBlank
+    private String subtitle;
+
+    @NotBlank
+    private String image;
+
+    @NotBlank
     private String author;
 
     @NotBlank
@@ -19,4 +24,42 @@ public class BooksDTO {
 
     @NotNull
     private String published;
+
+    public BooksDTO(String title, String author, String published, String description) {
+        this.title = title;
+        this.author = author;
+        this.published = published;
+        this.description = description;
+    }
+
+    public BooksDTO() {
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublished() {
+        return published;
+    }
+
+    public void setPublished(String published) {
+        this.published = published;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
